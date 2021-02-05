ccd9bc5bdbd3e4b30f7e3944cc3301a304f23994
Lana Karazoun <lkarazoun@usf.edu>
2021-02-05 17:47:01 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+    static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+            static void NameMethod()
+            {
+                string name = Console.ReadLine();
+                {
+                    Console.WriteLine("Hello " + name + "!");
+                }
+            }
+            NameMethod();         
+        }
+    }
+}
